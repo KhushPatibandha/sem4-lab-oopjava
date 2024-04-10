b835120d87a596bc4fecc78c86b47146ccc6f828
KhushPatibandha <khush.patibandha@gmail.com>
2024-04-10 09:43:57 +0530

    Lab9 added

---
+++ Lab9.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Lab9 {
+    public static void main(String[] args) {
+        // unchecked exception
+        try {
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+            System.out.println(c);
+        } catch (Exception e) {
+            System.err.println("Division by zero");
+        }
+
+        // checked exception
+        try {
+            FileReader file = new FileReader("file.txt");
+            BufferedReader br = new BufferedReader(file);
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("File not found");
+        }
+
+        // User defined exception
+        try {
+            int a = 10;
+            int b = 0;
+            int c = a / b;
+            if(b == 0) {
+                throw new ExceptionA("Division by zero");
+            }
+            System.out.println(c);
+        } catch (ExceptionA e) {
+            System.out.println(e.getMessage()); 
+        }
+    }
+}
+
+class ExceptionA extends Exception {
+    public ExceptionA(String message) {
+        super(message);
+    }
+}
